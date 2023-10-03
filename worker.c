05f385d5f4f2b582ee0d48068c78b5d52b4991ad
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-10-02 19:59:30 -0500

    Code to fill outline worker.c

--- worker.c
+++ worker.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
 
 #define SHMKEY 55555
 #define PERMS 0644
+#define MSGKEY 66666
+
 typedef struct msgbuffer {
         long mtype;
-        //char strData[100];
         int intData;
 } msgbuffer;
 
 int main(int argc, char** argv){
+
+        msgbuffer receiver;
+        receiver.mtype = 1;
+        int msqid = 0;
+
+        if ((msqid = msgget(MSGKEY, PERMS)) == -1) {
+                perror("msgget in child");
+                exit(1);
+        }
 
         int shmid = shmget(SHMKEY, sizeof(int)*2, 0777);
         if(shmid == -1){
@@ -34,23 +48,48 @@ int main(int argc, char** argv){
 
         int currentTime = sharedTime[0];
         int secondsPassed = 0;
-        //while (1)
-        //wait for message
-        while (exitTime[0] > sharedTime[0] || exitTime[1] > sharedTime[1]){// make if
-                if(sharedTime[0] - exitTime[0] >= 2){
+        while (1){
+                //wait for message
+                if ( msgrcv(msqid, &receiver, sizeof(msgbuffer), getpid(), 0) == -1) {
+                        perror("failed to receive message from parent\n");
+                        exit(1);
+                }
+                if (exitTime[0] > sharedTime[0] || exitTime[1] > sharedTime[1]){
+                        if(sharedTime[0] - exitTime[0] >= 2){
+                                //send message back that terminating
+                                receiver.mtype = getppid();
+                                receiver.intData = 0;
+                                if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
+                                        perror("msgsnd to parent failed\n");
+                                        exit(1);
+                                }
+                                break;
+                        }
+                        if(currentTime != sharedTime[0]){
+                                secondsPassed += sharedTime[0]-currentTime;
+                                printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d, %d SECONDS HAVE PASSED\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1],secondsPassed);
+                                currentTime = sharedTime[0];
+                        }
+                        //send message saying not terminating
+                        receiver.mtype = getppid();
+                        receiver.intData = 1;
+                        if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
+                                perror("msgsnd to parent failed\n");
+                                exit(1);
+                        }
+
+                }else{
                         //send message back that terminating
+                        receiver.mtype = getppid();
+                        receiver.intData = 0;
+                        if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
+                                perror("msgsnd to parent failed\n");
+                                exit(1);
+                        }
+
                         break;
                 }
-                if(currentTime != sharedTime[0]){
-                        secondsPassed += sharedTime[0]-currentTime;
-                        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d, %d SECONDS HAVE PASSED\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1],secondsPassed);
-                        currentTime = sharedTime[0];
-                }
-                //send message saying not terminating
-        }//else
-        //send message back that terminating
-        //break
-
+        }
         //printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d TERMINATING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
 
         shmdt(sharedTime);
